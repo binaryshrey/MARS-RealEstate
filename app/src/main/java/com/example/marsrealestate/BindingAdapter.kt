a379a688d2eb827488fb14ea41510b2473bee0b4
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2021-05-06 19:18:01 +0530

    adds DetailsFragment layout

--- app/src/main/java/com/example/marsrealestate/BindingAdapter.kt
+++ app/src/main/java/com/example/marsrealestate/BindingAdapter.kt
@@ -1,9 +1,13 @@
 package com.example.marsrealestate
 
+import android.view.View
 import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.example.marsrealestate.overview.MarsApiStatus
 
 
 @BindingAdapter("imageUrl")
@@ -15,3 +19,21 @@ fun bind(image : ImageView, imgUrl : String){
             .into(image)
     }
 }
+
+//@BindingAdapter("bind:marsApiStatus")
+//    fun status(progressBar: ProgressBar,errorTextView: TextView, status : MarsApiStatus){
+//        when(status){
+//            MarsApiStatus.LOADING ->{
+//                progressBar.visibility = View.VISIBLE
+//                errorTextView.visibility = View.INVISIBLE
+//            }
+//            MarsApiStatus.DONE -> {
+//                progressBar.visibility = View.INVISIBLE
+//                errorTextView.visibility = View.INVISIBLE
+//            }
+//            MarsApiStatus.ERROR -> {
+//                progressBar.visibility = View.INVISIBLE
+//                errorTextView.visibility = View.VISIBLE
+//            }
+//        }
+//    }
