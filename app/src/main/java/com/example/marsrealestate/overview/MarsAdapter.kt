4ca92a40474b08b019798d76667a411519a73010
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2021-05-06 16:05:33 +0530

    adds recyclerviewAdapter

---
+++ app/src/main/java/com/example/marsrealestate/overview/MarsAdapter.kt
@@ -0,0 +1,50 @@
+package com.example.marsrealestate.overview
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.marsrealestate.databinding.GridItemListViewBinding
+import com.example.marsrealestate.network.MarsProperty
+
+class MarsAdapter : ListAdapter<MarsProperty, MarsAdapter.ViewHolder>(DiffUtilCallback()) {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MarsAdapter.ViewHolder {
+        return ViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: MarsAdapter.ViewHolder, position: Int) {
+        val marsProperty = getItem(position)
+        holder.bind(marsProperty)
+    }
+
+    class ViewHolder private constructor(val binding : GridItemListViewBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(marsProperty: MarsProperty){
+            binding.property = marsProperty
+            binding.executePendingBindings()
+        }
+
+        companion object{
+            fun from(parent: ViewGroup) : ViewHolder{
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = GridItemListViewBinding.inflate(layoutInflater,parent, false)
+                return ViewHolder(binding)
+            }
+        }
+    }
+
+}
+class DiffUtilCallback : DiffUtil.ItemCallback<MarsProperty>(){
+    override fun areItemsTheSame(oldItem: MarsProperty, newItem: MarsProperty): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: MarsProperty, newItem: MarsProperty): Boolean {
+        return oldItem == newItem
+    }
+
+
+}
+
--- app/src/main/java/com/example/marsrealestate/overview/OverviewFragment.kt
+++ app/src/main/java/com/example/marsrealestate/overview/OverviewFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.marsrealestate.R
 import com.example.marsrealestate.databinding.FragmentOverviewBinding
@@ -14,6 +15,7 @@ class OverviewFragment : Fragment() {
 
     private lateinit var binding : FragmentOverviewBinding
     private lateinit var viewModel: OverviewViewModel
+    private lateinit var adapter: MarsAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,6 +26,15 @@ class OverviewFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater,R.layout.fragment_overview, container, false)
         viewModel = ViewModelProvider(this).get(OverviewViewModel::class.java)
         binding.overviewViewModel = viewModel
+
+        adapter = MarsAdapter()
+        binding.recyclerView.adapter = adapter
+
+        viewModel.property.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                adapter.submitList(it.toMutableList())
+            }
+        })
 
         return binding.root
     }
