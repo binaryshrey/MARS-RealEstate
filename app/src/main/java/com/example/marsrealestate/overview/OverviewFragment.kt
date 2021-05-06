6ed62c3fbbc85e57fb587460962ae4d8db91ed50
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2021-05-06 13:33:57 +0530

    adds navigation and dataBinding

--- app/src/main/java/com/example/marsrealestate/overview/OverviewFragment.kt
+++ app/src/main/java/com/example/marsrealestate/overview/OverviewFragment.kt
@@ -5,17 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.marsrealestate.R
+import com.example.marsrealestate.databinding.FragmentOverviewBinding
 
 class OverviewFragment : Fragment() {
 
+    private lateinit var binding : FragmentOverviewBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_overview, container, false)
+
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_overview, container, false)
+        return binding.root
     }
 
 
