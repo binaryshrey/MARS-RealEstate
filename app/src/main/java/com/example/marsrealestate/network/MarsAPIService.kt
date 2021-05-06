4c72e208fec6323901071b2fd308c99ce41f94eb
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2021-05-06 13:44:53 +0530

    adds MarsAPIService

---
+++ app/src/main/java/com/example/marsrealestate/network/MarsAPIService.kt
@@ -0,0 +1,35 @@
+package com.example.marsrealestate.network
+
+import androidx.lifecycle.LiveData
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Call
+import retrofit2.Retrofit
+
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
+import retrofit2.http.GET
+
+private const val BASE_URL= "https://mars.udacity.com/"
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+
+interface MarsAPIService{
+    @GET("realestate")
+    fun properties() :
+            Call<MarsProperty>
+}
+
+
+object MarsApi{
+    val retrofitService : MarsAPIService by lazy {
+        retrofit.create(MarsAPIService::class.java)
+    }
+}
